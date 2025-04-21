6e2c5665a0f51eda72b5034762470f296165094e
Thoma <Thoma@LAPTOP-LU9AH37M>
2025-04-21 11:24:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Program:    PCAD_Project_3.1.1
+// Date:       21 APR 2025
+// Programmer: Thomas A. Morrison
+
+using System;
+using System.Globalization;
+using System.Text;
+namespace PCAD_Project_3_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('0');
+            for (int i = 2; i <= 100; i+=2) 
+            {
+                sb.Append(' ');
+                sb.Append(i);
+            }
+            Console.WriteLine(sb.ToString() + "\n");
+            Console.Write("Press any key to continue. ");
+            Console.ReadKey();
+        }
+    }
+}
